2bb474c033c5a11707da9ce21f5bc1aca36237f9
tuann21061999-top <tuann21061999@gmail.com>
2026-05-11 15:34:37 +0700

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,22 +4,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: 'esnext',
+    target: 'es2020',
     minify: 'esbuild',
     cssCodeSplit: true,
-    reportCompressedSize: false, // Tăng tốc build
+    reportCompressedSize: false,
     chunkSizeWarningLimit: 1000,
-    assetsInlineLimit: 4096, // Inline các file nhỏ < 4kb vào CSS/JS để giảm request
+    assetsInlineLimit: 4096,
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) return 'vendor-react';
-            if (id.includes('lucide')) return 'vendor-ui';
-            if (id.includes('axios')) return 'vendor-utils';
-            return 'vendor'; // Các thư viện khác gộp vào vendor chung
-          }
-        }
+        // Tự động quản lý để tránh lỗi ReferenceError initialization
       }
     }
   }
